8023311271ea56408eb24a5aef79add58f2d44e4
jonahsaltzman <jonahsaltzman@gmail.com>
2021-12-22 19:07:03 -0500

    remove passport twitter strategy

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -135,44 +135,44 @@ passport.use(
     )
 )
 
-passport.use(
-	new TwitterStrategy(
-		{
-			consumerKey: TWT_KEY,
-			consumerSecret: TWT_SECRET,
-			callbackURL: TWT_CB_URL,
-		},
-		async function (token, tokenSecret, profile, done) {
-            console.log('in TWT strategy')
-            console.log('twt profile: ')
-            console.log(profile)
-			User.findOne({ twtId: profile.id }, async (err, user) => {
-                console.log('found user: ')
-                console.log(user)
-                if (user) {
-                    const update = {
-                        twtProfile: {
-                            profile: profile,
-                            token: token,
-                            tokenSecret: tokenSecret
-                        }
-                    }
-                    const newUser = await User.findByIdAndUpdate(
-                        user._id,
-                        update,
-                        {returnOriginal: false}
-                    )
-                    if (newUser.twtProfile.profile) {
-                        console.log('added twitter profile to user')
-                        return done(null, newUser, token, tokenSecret)
-                    } else {
-                        console.log('got twt tokens but failed to add to db')
-                        return done(null, null, null, null)
-                    }
-                }
-                console.log('could not find user linked to twt profile')
-				return done(err)
-			})
-		}
-	)
-)
+// passport.use(
+// 	new TwitterStrategy(
+// 		{
+// 			consumerKey: TWT_KEY,
+// 			consumerSecret: TWT_SECRET,
+// 			callbackURL: TWT_CB_URL,
+// 		},
+// 		async function (token, tokenSecret, profile, done) {
+//             console.log('in TWT strategy')
+//             console.log('twt profile: ')
+//             console.log(profile)
+// 			User.findOne({ twtId: profile.id }, async (err, user) => {
+//                 console.log('found user: ')
+//                 console.log(user)
+//                 if (user) {
+//                     const update = {
+//                         twtProfile: {
+//                             profile: profile,
+//                             token: token,
+//                             tokenSecret: tokenSecret
+//                         }
+//                     }
+//                     const newUser = await User.findByIdAndUpdate(
+//                         user._id,
+//                         update,
+//                         {returnOriginal: false}
+//                     )
+//                     if (newUser.twtProfile.profile) {
+//                         console.log('added twitter profile to user')
+//                         return done(null, newUser, token, tokenSecret)
+//                     } else {
+//                         console.log('got twt tokens but failed to add to db')
+//                         return done(null, null, null, null)
+//                     }
+//                 }
+//                 console.log('could not find user linked to twt profile')
+// 				return done(err)
+// 			})
+// 		}
+// 	)
+// )
